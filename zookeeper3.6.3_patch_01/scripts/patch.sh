d4e7cce28bbf91c9dec317bee283fa4f25efeb56
zhangpeng <pengzhang@yunify.com>
2025-04-02 11:19:37 +0800

    Fix: 修复重启删除目录的bug

---
+++ zookeeper3.6.3_patch_01/scripts/patch.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+cp /patch/zookeeper.sh /opt/app/bin/node/zookeeper.sh
+cp /patch/zookeeper.sh.tmpl /etc/confd/templates/zookeeper.sh.tmpl
---
+++ zookeeper3.6.3_patch_01/scripts/rollback.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+cp /patch/zookeeper.sh.tmpl.back /etc/confd/templates/zookeeper.sh.tmpl
